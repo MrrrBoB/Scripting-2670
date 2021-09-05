dd32d4f6fb8b316ce75a6014e919a8d61781ef7f
Nate <10805477@my.uvu.edu>
2021-09-04 21:57:39 -0600

    Wrote broken spawn code. Added rigidbody to floor so ball destroys on contact?

---
+++ PublishProject/New Unity Project/Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public GameObject spawnObject;
+    public float intermission;
+    // Start is called before the first frame update
+    //new Vector3(Random.Range(-8, 8), 8, Random.Range(-8, 8))
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+       
+    }
+    public IEnumerator spawn()
+    {
+        Instantiate(spawnObject, new Vector3(Random.Range(-8, 8), 8, Random.Range(-8, 8)), Quaternion.identity);
+        yield return new WaitForSeconds(intermission);
+    }
+}
