f27cd5fd7b7fd2c42478dac35b8b3bed07ac28ff
Nate <10805477@my.uvu.edu>
2021-09-10 21:22:42 -0600

    Plate now adopts materials of walls, messed around with emissions a bit

---
+++ PublishProject/New Unity Project/Assets/Scripts/AssignRandomMaterial.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignRandomMaterial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material[] matPool;
+    void Awake()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().material = matPool[Random.Range(0, 3)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
