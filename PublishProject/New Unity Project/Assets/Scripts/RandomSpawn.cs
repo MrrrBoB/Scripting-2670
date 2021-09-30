246d4faabf1827e40bc9bc3a7a40d508d70da053
Nate <10805477@my.uvu.edu>
2021-09-29 20:34:41 -0600

    Started putting together color selection

--- PublishProject/New Unity Project/Assets/Scripts/RandomSpawn.cs
+++ PublishProject/New Unity Project/Assets/Scripts/RandomSpawn.cs
@@ -7,18 +7,18 @@ public class RandomSpawn : MonoBehaviour
     public GameObject spawnObject;
     public float intermission;
     public bool activated = true;
-    public float StartIntermission;
+    private float startIntermission = 3f;
     public float spawnTime;
     public float speedIncrease;
 
     
     // Start is called before the first frame update
     //new Vector3(Random.Range(-8, 8), 8, Random.Range(-8, 8))
-    void Start()
+    void Awake()
     {
        
         StartCoroutine(spawn());
-        intermission = StartIntermission;
+        intermission = startIntermission;
     }
 
     // Update is called once per frame
