4a7c2a0df9ffafbc2d4aaafb432ca24f25ff2f46
Nate <10805477@my.uvu.edu>
2021-10-09 15:38:49 -0600

    Game now pauses on gameover. Need to tidy up that script and double check the transition between menu and new level

--- PublishProject/New Unity Project/Assets/Scripts/GamePause.cs
+++ PublishProject/New Unity Project/Assets/Scripts/GamePause.cs
@@ -7,11 +7,19 @@ public class GamePause : MonoBehaviour
 {
     // Start is called before the first frame update
     public Button menuButton;
-
+    public bool gameIsPaused;
     public void PauseToggle(Toggle obj)
     {
         Time.timeScale = obj.isOn ? 0 : 1;
         AudioListener.pause = obj.isOn;
         menuButton.enabled = obj.isOn;
+        gameIsPaused = obj.isOn;
+    }
+
+    public void ChangePause()
+    {
+        Time.timeScale = gameIsPaused ? 1 : 0;
+        gameIsPaused = !gameIsPaused;
+        Debug.Log("Game End Pause");
     }
 }
