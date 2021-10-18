fa0c3484dc4f28218df581ce17ffd0f195fa67a0
Nathan <nathoward22@gmail.com>
2021-10-18 11:01:51 -0600

    Imported music into assets and created playlist. Need to implement menu

---
+++ PublishProject/New Unity Project/Assets/Scripts/MusicPlaylist.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MusicPlaylist : ScriptableObject
+{
+    public List<AudioClip> SongList;
+    public AudioClip selectedSong;
+
+    public void setSong(int i)
+    {
+        selectedSong = SongList[i];
+    }
+
+}
