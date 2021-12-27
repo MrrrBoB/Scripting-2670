cf763d6a83637df6de970ebcbee3117423094d2f
Nate <10805477@my.uvu.edu>
2021-12-27 10:58:26 -0700

    Added locating beam to balls

--- PublishProject/New Unity Project/Assets/Scripts/AssignRandomMaterial.cs
+++ PublishProject/New Unity Project/Assets/Scripts/AssignRandomMaterial.cs
@@ -15,5 +15,7 @@ public class AssignRandomMaterial : MonoBehaviour
         this.gameObject.GetComponent<MeshRenderer>().material =
             colorPool.GetMat(Random.Range(0, colorPool.colorList.Count ));
         gameObject.transform.GetChild(0).gameObject.GetComponent<Light>().color = gameObject.GetComponent<MeshRenderer>().material.color;
+        gameObject.transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material =
+            this.gameObject.GetComponent<MeshRenderer>().material;
     }
 }
